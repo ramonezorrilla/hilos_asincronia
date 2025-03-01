d6c0a4d3c240366f719ea78c5889262367172d76
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-03-01 14:42:48 -0400

    cambio con mis estudiantes de programacion basica para entender como funciona mas de una tarea asincrona

--- hilos_asincronia/Program.cs
+++ hilos_asincronia/Program.cs
@@ -6,15 +6,23 @@ namespace Tareas
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         { 
-            var task = new Task(() =>
+            var task = new Task(() => // Crear una tarea asincrona con un lambda como parametro
             {
                 Thread.Sleep(1000);
                 Console.WriteLine("Esta es la tarea asincrona con hilos");
             });
             task.Start();
 
+            var task2 = new Task(() => // Crear una tarea asincrona con un lambda como parametro
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Esta es la tarea asincrona con hilos 2");
+            });
+            task2.Start();
+            await  task; // Espera a que la tarea termine
+            await task2; // Espera a que la tarea termine
             Console.WriteLine("Esta es la tarea sincrona");
             Console.ReadKey();
 
