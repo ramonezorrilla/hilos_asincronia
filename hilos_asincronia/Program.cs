b8c1ff0cf595e0daeae11b61b3fff852813fbc3b
ramonezorrilla <ramoenzorrilla@gmail.com>
2025-03-01 14:30:20 -0400

    Add project files.

---
+++ hilos_asincronia/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tareas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var task = new Task(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Esta es la tarea asincrona con hilos");
+            });
+            task.Start();
+
+            Console.WriteLine("Esta es la tarea sincrona");
+            Console.ReadKey();
+
+        }
+    
+    
+    
+    }
+
+
+
+}
